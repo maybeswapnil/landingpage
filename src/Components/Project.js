a85ab5eed2c093c905d63d1554871c419249d0e3
Swapnil Sharma <swapnil.sharma1998@outlook.com>
2023-08-23 20:17:20 +0530

    Npm logo and project add

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -33,12 +33,13 @@ function Projects(props) {
       <div className="project-image-div">
         <p className='underlined'>{props.description}</p>
         <h1>{props.name}</h1>
-        <p className='des-para'>{props.descriptionpara}</p>
+        
+        {props.descriptionpara.length>150?<p className='des-para'>{props.descriptionpara.slice(0, 150) + " ..."}</p>:<p className='des-para'>{props.descriptionpara}</p>}
         <p className='underlined-github'><a href={props.url} target='_blank' id='git-link'>Github</a></p>
-        <div class="outer">
-          <img src={ary[props.index]} width={'60px'} />
-        </div>
       </div>
+        <div className="outer">
+          <img src={ary[props.index]} width={'60px'} height={'60px'}/>
+        </div>
     </div>
   );
 }
